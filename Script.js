60103d66259da068b3ef320fa4cf1e6bddb678bb
Arekuso-kun <alexodrino@gmail.com>
2022-10-25 20:26:25 +0300

    v0.4

--- Script.js
+++ Script.js
@@ -8,12 +8,20 @@ var mS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct
 
 setInterval(function () {
     const d_now = new Date();
-    // const d_now = new Date(d.getFullYear(), 0, 0, 0, 0, 0, 0);
+    const d_christmas = new Date(d.getFullYear(), 11, 25, 0, 0, 0, 0);
+    const d_last_christmas = new Date(d.getFullYear() - 1, 11, 25, 0, 0, 0, 0);
 
-    // d_now.setDate(31);
+    // d_now.setMonth(11);
+    // d_now.setDate(30);
     // d_now.setHours(23);
     // d_now.setMinutes(59);
     // d_now.setSeconds(59);
+
+    // d_now.setMonth(0);
+    // d_now.setDate(1);
+    // d_now.setHours(0);
+    // d_now.setMinutes(0);
+    // d_now.setSeconds(0);
 
     let miliseconds = (d_now.getTime() - d_zero.getTime()) % 1000;
     let seconds = d_now.getSeconds() + miliseconds / 1000;
@@ -27,7 +35,7 @@ setInterval(function () {
     let days_month = d_now.getDate() - 1 + hours / 24;
     let days_year = -1 + hours / 24;
 
-    document.getElementsByClassName("second-text")[0].innerHTML = "Second: " + Number(miliseconds / 10).toFixed(2) + "%";
+    document.getElementsByClassName("second-text")[0].innerHTML = d_now.getMonth() + "Second: " + Number(miliseconds / 10).toFixed(2) + "%";
     document.getElementsByClassName("second-bar")[0].style.width = miliseconds / 10 + "%";
 
     document.getElementsByClassName("minute-text")[0].innerHTML = "Minute: " + Number(seconds * (100 / 60)).toFixed(2) + "%";
@@ -44,8 +52,8 @@ setInterval(function () {
 
     let month_lenght = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     if (d_now.getFullYear() % 4 == 0) month_lenght[1] = 29;
-    document.getElementsByClassName("month-text")[0].innerHTML = "Month: " + Number(days_month * (100 / (month_lenght[d_now.getMonth()] - 1))).toFixed(2) + "%";
-    document.getElementsByClassName("month-bar")[0].style.width = days_month * (100 / (month_lenght[d_now.getMonth()] - 1)) + "%";
+    document.getElementsByClassName("month-text")[0].innerHTML = "Month: " + Number(days_month * (100 / (month_lenght[d_now.getMonth()]))).toFixed(2) + "%";
+    document.getElementsByClassName("month-bar")[0].style.width = days_month * (100 / (month_lenght[d_now.getMonth()])) + "%";
 
     for (let i = 0; i < d_now.getMonth(); i++) {
         days_year += month_lenght[i];
@@ -53,8 +61,8 @@ setInterval(function () {
     days_year += d_now.getDate();
     let year_lenght = 365;
     if (d_now.getFullYear() % 4 == 0) year_lenght = 366;
-    document.getElementsByClassName("year-text")[0].innerHTML = "Year: " + Number(days_year * (100 / (year_lenght - 1))).toFixed(2) + "%";
-    document.getElementsByClassName("year-bar")[0].style.width = days_year * (100 / (year_lenght - 1)) + "%";
+    document.getElementsByClassName("year-text")[0].innerHTML = "Year: " + Number(days_year * (100 / (year_lenght))).toFixed(2) + "%";
+    document.getElementsByClassName("year-bar")[0].style.width = days_year * (100 / (year_lenght)) + "%";
 
     document.getElementsByClassName("week_day")[0].innerHTML = wL[d_now.getDay()];
 
@@ -66,4 +74,9 @@ setInterval(function () {
         mm = "0" + mm;
     document.getElementsByClassName("clock")[0].innerHTML = hh + ":" + mm;
     document.getElementsByClassName("date")[0].innerHTML = d_now.getDate() + " " + mS[d_now.getMonth()] + " " + d_now.getFullYear();
+
+    let one_year = d_christmas.getTime() - d_last_christmas.getTime();
+    document.getElementsByClassName("christmas-text")[0].innerHTML = "Christmas: " + Number((d_now.getTime() - d_zero.getTime()) * (100 / one_year)).toFixed(2) + "%";
+    document.getElementsByClassName("christmas-bar")[0].style.width = days_year * (100 / (year_lenght)) + "%";
+    document.getElementsByClassName("christmas-text2")[0].innerHTML = Number(year_lenght-days_year-7).toFixed(2) + " days until Christmas !";
 }, 1);
